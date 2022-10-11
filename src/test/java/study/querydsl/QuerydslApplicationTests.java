435278bf1c3b7e774571c6897c4920949d22ec2e
hesung <hsjo6864@gmail.com>
2022-10-11 13:52:58 +0900

    2022.10.11 Querydsl Study Project - 프로젝트 기본 환경 세팅 - yml 파일 및 p6spy 포매터 추가

--- src/test/java/study/querydsl/QuerydslApplicationTests.java
+++ src/test/java/study/querydsl/QuerydslApplicationTests.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
 import study.querydsl.entity.Hello;
 import study.querydsl.entity.QHello;
@@ -22,6 +23,7 @@ class QuerydslApplicationTests {
 	EntityManager em;
 
 	@Test
+	@Commit
 	void contextLoads() {
 		Hello hello = new Hello();
 		em.persist(hello);
